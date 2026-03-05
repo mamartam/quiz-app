62bca37c8917bd64db3d1cc082b8dc135e2ba34b
mamartam <marta.masiukk@gmail.com>
2026-03-05 19:00:12 +0200

    create file for variables from dom and collect them like an object so it will be easy to manupulate them

---
+++ js/dom-variables.js
@@ -0,0 +1,35 @@
+export const DOM_VAR = {
+  // welcome page section
+  startBtn: document.querySelector(".start-btn"),
+  welcomePageSection: document.querySelector(".welcome-page-section"),
+  chooseTopicSection: document.querySelector(".choose-topic-section"),
+  // choose topic section
+  topics: document.querySelector(".topics-container"),
+  goToTheComplexitySectionBtn: document.querySelector(
+    ".go-to-the-complexity-section-btn",
+  ),
+  topicBtns: document.querySelectorAll(".topic-btn"),
+  // choose complexity level section
+  backToTheTopicsBtn: document.querySelector(".back-to-the-topics-btn"),
+  startQuiz: document.querySelector(".start-quiz"),
+  complexityBtnsContainer: document.querySelector(".complexity-btns-container"),
+  complexitySection: document.querySelector(".complexity-section"),
+  complexityButtons: document.querySelectorAll(".complexity-btn"),
+
+  // quiz section
+  quizSection: document.querySelector(".quiz-section"),
+  quizInfoTopic: document.querySelector(".quiz-info-topic"),
+  quizInfoLevel: document.querySelector(".quiz-info-level"),
+
+  numberOfQuestion: document.querySelector(".number-of-question"),
+  questionHeader: document.querySelector(".question-header"),
+  answersOptionsArray: document.querySelectorAll(".option"),
+  answersOptionContainer: document.querySelector(".answers-option-container"),
+  // const nextQuestionBtn = document.querySelector(".next-question-btn");
+  // const finishBtn = document.querySelector(".finish-btn");
+  userPoints: document.querySelector(".user-points"),
+
+  // Result section
+  resultsSection: document.querySelector(".results-section"),
+  playAgainBtn: document.querySelector(".play-again"),
+};
